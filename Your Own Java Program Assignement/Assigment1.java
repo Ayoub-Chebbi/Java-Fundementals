d5f9c397af9332b53468c172f0932644c39cbdbe
siayouba <117870003+siayouba@users.noreply.github.com>
2024-02-27 21:10:47 +0100

    First Java Assignement

---
+++ Your Own Java Program Assignement/Assigment1.java
@@ -0,0 +1,7 @@
+public class Assigment1{
+    public static void main(String[] args){
+        System.out.println("My name is Ayoub Chebbi");
+        System.out.println("I am 18 years old");
+        System.out.println("My home town is Chebba");
+    }
+}
