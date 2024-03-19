cc30fe3cc905fa789425a01bea50855419401be9
siayouba <117870003+siayouba@users.noreply.github.com>
2024-03-19 22:06:17 +0100

    Hoppers Receipt

---
+++ Java-Spring/Hoppers Receipt/Hopers-Receipt/src/main/java/com/example/hopers/HopersReceiptApplication.java
@@ -0,0 +1,13 @@
+package com.example.hopers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HopersReceiptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HopersReceiptApplication.class, args);
+	}
+
+}
