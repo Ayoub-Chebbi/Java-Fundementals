9786435803936297f6cb2d103ac86b4909a2d9c8
Ayouba <ayoubchebbi570@gmail.com>
2024-03-28 23:17:07 +0100

    Rendering Books

---
+++ Java-Spring/books Api/books-api/src/main/java/com/example/books/controllers/BookController.java
@@ -0,0 +1,28 @@
+package com.example.books.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import com.example.books.services.BookService;
+
+import org.springframework.ui.Model;
+
+@Controller
+public class BookController {
+	@Autowired
+	BookService bookService;
+	
+	@GetMapping("/")
+	public String home() {
+		return "redirect:/books";
+	}
+	
+	@GetMapping("/books/{bookId}")
+	public String index(Model model, @PathVariable("bookId") Long bookId) {
+		
+        model.addAttribute("book", bookService.findBook(bookId));
+        return "show.jsp";
+	}
+
+}
