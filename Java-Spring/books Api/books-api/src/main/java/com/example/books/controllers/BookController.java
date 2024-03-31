11aca28c4aadce7b729ee782e789f0397dc035cf
Ayouba <ayoubchebbi570@gmail.com>
2024-03-31 10:30:36 +0100

    All books

--- Java-Spring/books Api/books-api/src/main/java/com/example/books/controllers/BookController.java
+++ Java-Spring/books Api/books-api/src/main/java/com/example/books/controllers/BookController.java
@@ -1,9 +1,14 @@
 package com.example.books.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.books.models.Book;
 import com.example.books.services.BookService;
 
 import org.springframework.ui.Model;
@@ -12,6 +17,7 @@ import org.springframework.ui.Model;
 public class BookController {
 	@Autowired
 	BookService bookService;
+	
 	
 	@GetMapping("/")
 	public String home() {
@@ -23,6 +29,13 @@ public class BookController {
 		
         model.addAttribute("book", bookService.findBook(bookId));
         return "show.jsp";
-	}
+	}        
+    @GetMapping("/books")
+    public String index(Model model) {
+    	List<Book> books = bookService.allBooks();
+        model.addAttribute("books", books);
+        return "index.jsp";
+    	}
 
 }
+
